6a955385fbbe66834dda744a840be9c5616fb6aa
Malachi Mart <malachimart@gmail.com>
2022-08-11 16:44:36 -0500

    initial update for migration from vim-plug to packer and a lua centric approach to nvim

---
+++ nvim/.config/nvim/lua/config/init.lua
@@ -0,0 +1,37 @@
+require("config.set")
+require("config.packer")
+
+local augroup = vim.api.nvim_create_augroup
+SvikariGroup = augroup('SvikariGroup', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufEnter", "BufWinEnter", "TabEnter"}, {
+    group = SvikariGroup,
+    pattern = "*.rs",
+    callback = function()
+        require("lsp_extensions").inlay_hints{}
+    end
+})
+
+autocmd({"BufWritePre"}, {
+    group = SvikariGroup,
+    pattern = "*",
+    command = "%s/\\s\\+$//e",
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
